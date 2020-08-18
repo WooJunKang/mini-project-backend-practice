b539506145def242161022fea58e131f80d66028
woojunkang <woojunkang@woojunkangui-MacBook-Pro.local>
2020-08-18 21:11:17 +0900

    succeed getting data and create tweet

---
+++ app.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const User = require('./models/userSchema');
+const Contents = require('./models/contentsSchema');
+const bodyParser = require('body-parser');
+const fetch = require("node-fetch");
+
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json());
+
+// connect web server - DB
+
+app.listen(3001);
+const dbURI = 'mongodb+srv://woojun-kang:1234@my-first-cluster.9pf89.mongodb.net/twittler?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log('connected to db'))
+  .catch((err) => console.log(err));
+
+
+  
+
+app.get('/', (req, res) => {
+  res.sendFile('./index.html', {root : __dirname });
+})
+
+
+// get all TWEET from DB
+app.get('/contents', (req, res) => {
+  console.log(111);
+  Contents.find({})
+  .exec(function(err, docs){
+    if(err){
+      console.log('!!! error !!!');
+    } else {
+      res.json(docs);
+    }
+  })
+})
+
+const userAction = async () => {
+  fetch('http://localhost:3001/contents')
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }
+  userAction();
+
+
+
+// create TWEET 
+app.post('/content', (req, res) => {
+  console.dir(req);
+  console.log(req.body);
+  const { username } = req.body;
+  const { userpw } = req.body;
+  const { content } = req.body;
+  const createdAt = new Date();
+
+  // content collection
+  const  newContent = new Contents({
+    user_name: username,
+    password: userpw,
+    content: content,
+    created_at: createdAt,
+    is_deleted: false,
+    deleted_at: null
+  })
+
+  // user collection
+  const newUser = new User({
+    user_name: username,
+    is_admin: false
+  })
+
+  newContent.save()
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+  newUser.save()
+  .then((result) => {
+    res.send(result)
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+  // res.sendFile('./index.html', {root : __dirname });
+
+})
+
+
+
+
+
+
+
